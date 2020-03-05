43bd4dddf6de6f226269c412b23c2a4946413317
andrew <andrewsafwatsamuel@gmail.com>
2020-03-05 23:26:13 +0200

    implemented NewsViewModel

--- app/src/main/java/com/andrew/newsapp/domain/UseCases.kt
+++ app/src/main/java/com/andrew/newsapp/domain/UseCases.kt
@@ -14,20 +14,13 @@ class RefreshTopStoriesUseCase(private val repository: TopStoriesRepository = to
         type: String,
         state: MutableLiveData<TopStoriesState>
     ) = repository
-        .takeIf { if (!isConnected) state.value = Error("Check your internet connection");isConnected }
+        .takeIf {
+            if (!isConnected) state.value = Error("Check your internet connection");isConnected
+        }
         ?.also { state.value = Loading }
         ?.run { refreshNews(type) }
         ?.let { state.value = if (it == 200) Success else Error("Error While Loading") }
 }
 
-class GetTopStoriesUseCase(private val repository: TopStoriesRepository = topStoriesRepository) {
-    operator fun invoke(
-        state: MutableLiveData<TopStoriesState>
-    ) = repository
-        .also { state.value = Loading }
-        .retrieveNews()
-        .also {
-            state.value =
-                if (it.value.isNullOrEmpty()) Error("No Stores To Show Please check your Connection Then Try Again") else Success
-        }
-}
+fun getTopStories(repository: TopStoriesRepository = topStoriesRepository) =
+    repository.retrieveNews()
