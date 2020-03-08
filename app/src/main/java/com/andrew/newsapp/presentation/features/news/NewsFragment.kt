c29eca9a993526225f275052caf20cb902862e95
andrew <andrewsafwatsamuel@gmail.com>
2020-03-08 23:17:51 +0200

    drawn story_list_item

--- app/src/main/java/com/andrew/newsapp/presentation/features/news/NewsFragment.kt
+++ app/src/main/java/com/andrew/newsapp/presentation/features/news/NewsFragment.kt
@@ -5,19 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.andrew.newsapp.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory
 
-/**
- * A simple [Fragment] subclass.
- */
+import com.andrew.newsapp.R
+import com.andrew.newsapp.domain.*
+
 class NewsFragment : Fragment() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, NewInstanceFactory())[NewsViewModel::class.java]
+    }
+    private var index = 0
+    private val types = listOf(ARTS, HOME_STORIES, SCIENCE, U_S, WORLD)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
-    }
+    ): View = inflater.inflate(R.layout.fragment_news, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //viewModel.topStories.observe(viewLifecycleOwner, Observer {
+
+        //})
+    }
 }
+
+
