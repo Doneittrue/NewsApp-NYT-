3edca92684a129431f0f08883036a48a4df25d8b
andrew <andrewsafwatsamuel@gmail.com>
2020-03-10 19:45:00 +0200

    added photos to top stories

--- app/src/main/java/com/andrew/newsapp/presentation/Engine.kt
+++ app/src/main/java/com/andrew/newsapp/presentation/Engine.kt
@@ -3,7 +3,15 @@ package com.andrew.newsapp.presentation
 import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.andrew.newsapp.R
+import com.bumptech.glide.Glide
 
 fun Activity.checkConnectivity(): Boolean =
-(getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
-.run { activeNetworkInfo != null && activeNetworkInfo.isConnected }
+    (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+        .run { activeNetworkInfo != null && activeNetworkInfo.isConnected }
+
+@BindingAdapter("loadImageFromServer")
+fun ImageView.loadImageFromServer(url: String?) = url
+    .let { Glide.with(context).load(it).placeholder(R.drawable.place_holder).into(this) }
